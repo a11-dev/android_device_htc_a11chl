eb0f421250d2448090c0f9119b59a1ab405ce01c
Greg Wilard <wickett06@gmail.com>
2016-05-21 18:40:26 -0400

    a11chl: don't assert the releasetools

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -18,5 +18,5 @@
 
 def FullOTA_InstallEnd(info):
   info.script.Mount("/system")
-  info.script.AppendExtra('assert(run_program("/tmp/install/bin/variant_script.sh") == 0);')
+  info.script.AppendExtra('run_program("/tmp/install/bin/variant_script.sh");')
   info.script.Unmount("/system")
